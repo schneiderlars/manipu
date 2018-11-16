f5f741c4aaf90d19f6d0324bc4d5cedbb01e406e
reun <r.unrau@conterra.de>
2018-11-17 00:33:33 +0100

    enable multi bundle parsing

--- src/PumlConverter.js
+++ src/PumlConverter.js
@@ -1,42 +1,65 @@
 module.exports.bundleToPuml = function (bundle) {
-    if(!bundle){
-        return `@startuml
+  if (!bundle) {
+    return `@startuml
         title Bundle Diagram
         @enduml`
-    }
-    return `@startuml
+  }
+  return `@startuml
     title Bundle Diagram
     package "${bundle.getName()}" {
         ${createComponents(bundle)}
     }
-    ${createReferences(bundle)}
+    ${createReferences([bundle], bundle.getComponents())}
     @enduml `
-}
+};
+
+module.exports.bundlesToPuml = function (bundles) {
+  if (!bundles) {
+    return `@startuml
+        title Bundle Diagram
+        @enduml`
+  }
+  let bundlesString = `@startuml
+    title Bundle Diagram`;
+  for (let bundle of bundles) {
+    bundlesString += `package "${bundle.getName()}" {
+      ${createComponents(bundle)}
+    }`
+  }
+  for (let bundle of bundles) {
+    bundlesString += `${createReferences(bundles, bundle.getComponents())}`
+  }
+  bundlesString += `@enduml`;
+  return bundlesString;
+};
 
 function createComponents(bundle) {
-    let result = "";
-    const components = bundle.getComponents();    
-    for (let component of components) {
-        let interfaces = component.getInterfaces();
-        let interfacesString = interfaces.length ? ` <<${interfaces.join(",")}>>` : ""
-        result += `[${component.getName()}]${interfacesString}
+  let result = "";
+  const components = bundle.getComponents();
+  for (let component of components) {
+    let interfaces = component.getInterfaces();
+    let interfacesString = interfaces.length ? ` <<${interfaces.join(",")}>>` : ""
+    result += `[${component.getName()}]${interfacesString}
         `;
-    }    
-    return result;
+  }
+  return result;
 }
 
-function createReferences(bundle) {
-    let result = "";
-    const components = bundle.getComponents();    
-    for (let component of components) {
-        const references = component.getReferences();
-        for (let interface of references) {
-            const refComponents = bundle.getComponentsByInterface(interface);
-            for (let refComponent of refComponents) {
-                result += `[${component.getName()}] --> [${refComponent.getName()}]
+function createReferences(bundles, components) {
+  let result = "";
+  for (let component of components) {
+    const references = component.getReferences();
+    for (let interface of references) {
+      const refComponents = getComponentsByInterface(bundles, interface);
+      for (let refComponent of refComponents) {
+        result += `[${component.getName()}] --> [${refComponent.getName()}]
                 `;
-            }            
-        }                
-    }    
-    return result;
+      }
+    }
+  }
+  return result;
+}
+
+function getComponentsByInterface(bundles, interface) {
+  return bundles[0].getComponentsByInterface(interface);
 }
--- test/PumlConverter.test.js
+++ test/PumlConverter.test.js
@@ -31,8 +31,42 @@ describe('PumlConverter', function () {
             bundle.addComponent(c1);
             bundle.addComponent(c2);
             const result = PumlConverter.bundleToPuml(bundle);            
-            assert(result.includes(`[Component1] -> [Component2]`));
+            assert(result.includes(`[Component1] --> [Component2]`));
             assert(result.includes(`[Component2] <<i2>>`));
+        });
+    });
+    describe('bundlesToPuml', function () {
+        it('should return puml with title for empty bundle list', function () {
+            const result = PumlConverter.bundlesToPuml();
+            assert(result.includes(`title Bundle Diagram`));
+        });
+        it('should return puml with bundle for single bundle', function () {
+          const bundle = new Bundle("map-init");
+          const c1 = new Component("Component1", ["i1"], ["i2"]);
+          const c2 = new Component("Component2", ["i2"]);
+          bundle.addComponent(c1);
+          bundle.addComponent(c2);
+          const result = PumlConverter.bundlesToPuml([bundle]);
+          assert(result.includes(`[Component1] --> [Component2]`));
+          assert(result.includes(`[Component2] <<i2>>`));
+        });
+        it('should return puml with bundle for multiple bundles', function () {
+          const bundle1 = new Bundle("map-init");
+          const c1 = new Component("Component1", ["i1"], ["i2"]);
+          const c2 = new Component("Component2", ["i2"]);
+          bundle1.addComponent(c1);
+          bundle1.addComponent(c2);
+
+          const bundle2 = new Bundle("map-widget");
+          const c3 = new Component("Component3", ["i3"], ["i4"]);
+          const c4 = new Component("Component4", ["i4"], ["i1", "i2"]);
+          bundle1.addComponent(c3);
+          bundle1.addComponent(c4);
+
+          const result = PumlConverter.bundlesToPuml([bundle1, bundle2]);
+          assert(result.includes(`[Component3] --> [Component4]`));
+          assert(result.includes(`[Component4] --> [Component1]`));
+          assert(result.includes(`[Component4] --> [Component2]`));
         });
     });
 });
